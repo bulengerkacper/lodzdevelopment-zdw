30db52543343fb07aee418e1d628f836b5be20d7
bulengerkacper <bulenger.kacper@gmail.com>
2020-11-20 23:28:14 +0100

    Refactored in order to include rozana

--- utils/json_generator_for_maps/generator.py
+++ utils/json_generator_for_maps/generator.py
@@ -1,8 +1,8 @@
 import requests
 import json
 import time
-print ("Podaj nazwe pliku z ulicami")
-filename=input()
+print ("Podaj nazwe pliku")
+filename = input()
 with open(filename, encoding="utf8") as f:
     lines = f.readlines()
 
@@ -11,11 +11,13 @@ for line in lines:
     network.append(line)
 
 for street in network:
-    preparedlink = "https://nominatim.openstreetmap.org/search/"+street+"%20Zdu%C5%84ska%20Wola?format=json&addressdetails=1&limit=1&polygon_svg=1"
+    
+    #preparedlink = "https://nominatim.openstreetmap.org/search/"+street+"%20Zdu%C5%84ska%20Wola?format=json&addressdetails=1&limit=1&polygon_svg=1"
+    preparedlink = "https://nominatim.openstreetmap.org/search/?format=json&addressdetails=1&limit=1&polygon_svg=1&street="+street+"&city=Zdu%C5%84ska%20Wola"
     preparedlink = preparedlink.replace('\n','')
     r = requests.get(preparedlink)
     data = json.loads(r.text)
-    f = open(filename+".jsoned","a", encoding="utf8")
+    f = open(filename +".json","a", encoding="utf8")
     for value in data:
         #print(value["svg"])
         data2=value["address"]
