a0cc848cb02f011cbb632dbd9c68103c4a72e6c7
bulengerkacper <bulenger.kacper@gmail.com>
2020-11-20 20:39:37 +0100

    Creating links for request

--- utils/json_generator_for_maps/generator.py
+++ utils/json_generator_for_maps/generator.py
@@ -1,8 +1,18 @@
+import requests
+
 with open('siec_gazowa.txt', encoding="utf8") as f:
     lines = f.readlines()
 
+network=[]
 for line in lines:
-    print(line)
+    network.append(line)
+
+for street in network:
+    #print(street)
+    preparedlink = "https://nominatim.openstreetmap.org/search/"+street +"%20Zdu%C5%84ska%20Wola?format=json&addressdetails=1&limit=1&polygon_svg=1"
+    print(preparedlink)
+    #r = requests.get("https://nominatim.openstreetmap.org/search/" + street + "%20Zdu%C5%84ska%20Wola?format=json&addressdetails=1&limit=1&polygon_svg=1")
+
 
 
 
