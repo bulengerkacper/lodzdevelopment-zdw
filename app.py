c58588e66a3ff9905706a38218ca30c92ffa6c0b
Rafal Staniaszek <doxter@protonmail.com>
2020-11-20 20:21:33 +0100

    Utils directory and flask route for testing

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect
 from flask import request
+from utils import calc
 
 application = Flask(__name__)
 
@@ -7,6 +8,11 @@ application = Flask(__name__)
 def main():
     return render_template("index.html")
 
+@application.route('/test')  
+def home():  
+    calc.test()
+    return "hello, welcome to our website";  
+
 if __name__ == "__main__":
     application.config['TEMPLATES_AUTO_RELOAD'] = True
-    application.run(debug=True, host='0.0.0.0')
+    application.run(debug=True, host='127.0.0.1')
