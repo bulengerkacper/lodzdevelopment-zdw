b154eef983c116018795204bc6864e099108d070
bulengerkacper <bulenger.kacper@gmail.com>
2020-11-20 21:51:04 +0100

    Further work on generating jsons

--- utils/json_generator_for_maps/generator.py
+++ utils/json_generator_for_maps/generator.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import time
 
 with open('siec_gazowa.txt', encoding="utf8") as f:
     lines = f.readlines()
@@ -8,11 +10,14 @@ for line in lines:
     network.append(line)
 
 for street in network:
-    #print(street)
-    preparedlink = "https://nominatim.openstreetmap.org/search/"+street +"%20Zdu%C5%84ska%20Wola?format=json&addressdetails=1&limit=1&polygon_svg=1"
-    print(preparedlink)
-    #r = requests.get("https://nominatim.openstreetmap.org/search/" + street + "%20Zdu%C5%84ska%20Wola?format=json&addressdetails=1&limit=1&polygon_svg=1")
-
-
-
-
+    preparedlink = "https://nominatim.openstreetmap.org/search/"+street+"%20Zdu%C5%84ska%20Wola?format=json&addressdetails=1&limit=1&polygon_svg=1"
+    preparedlink = preparedlink.replace('\n','')
+    r = requests.get(preparedlink)
+    data = json.loads(r.text)
+    for x in data:
+        #print(x["svg"])
+        print (x["address"])
+        # data2=json.loads(x["address"].dump())
+        # for z in data2:
+        #     print(z["roads"])
+        
