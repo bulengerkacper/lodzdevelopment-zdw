4e395aca105269e1190767b61b8393149c4934e3
bulengerkacper <bulenger.kacper@gmail.com>
2020-11-21 15:30:28 +0100

    limit range changed to 10

--- utils/json_generator_for_maps/generator.py
+++ utils/json_generator_for_maps/generator.py
@@ -15,7 +15,7 @@ json_bufer = open(filename +".jsons","a", encoding="utf8")
 print("[")
 json_bufer.write("[")
 for street in network:
-    preparedlink = "https://nominatim.openstreetmap.org/search/?format=json&addressdetails=1&limit=2&polygon_svg=1&street="+street+"&city=Zdu%C5%84ska%20Wola"
+    preparedlink = "https://nominatim.openstreetmap.org/search/?format=json&addressdetails=1&limit=10&polygon_svg=1&street="+street+"&city=Zdu%C5%84ska%20Wola"
     preparedlink = preparedlink.replace('\n','')
     r = requests.get(preparedlink)
     json_data = json.loads(r.text)
@@ -23,26 +23,24 @@ for street in network:
         address_data=value["address"]
         print("{'road':'" + address_data["road"] + "','points':[")
         json_bufer.write("{'road':'" + address_data["road"] + "','points':[")
-        splitted = value["svg"].split("L ",1)
+        splitted = value["svg"].split("L ")
         print(splitted)
-        splitSecondSpace=splitted[1].split(" ")
-        result = [splitSecondSpace[index] + ' ' + splitSecondSpace[index+1] for index in range(len(splitSecondSpace)-1)]
-        i=0
-        print("'" + splitted[0][2::][:-1] + "',")
-        json_bufer.write("'" + splitted[0][2::][:-1] + "',")
-        for pair in result:
-            i=i+1
-            if i%2==1:
-                print ("'" + pair + "',") #tu printuje pary
-                json_bufer.write("'" + pair + "',")   
-        print("]")
+        if len(splitted) > 1:
+            splitSecondSpace=splitted[1].split(" ")
+            result = [splitSecondSpace[index] + ' ' + splitSecondSpace[index+1] for index in range(len(splitSecondSpace)-1)]
+            i=0
+            #print("'" + splitted[0][2::][:-1] + "',")
+            json_bufer.write("'" + splitted[0][2::][:-1] + "',")
+            for pair in result:
+                i=i+1
+                if i%2==1:
+                    #print ("'" + pair + "',") #tu printuje pary
+                    json_bufer.write("'" + pair + "',")   
+        #print("]")
         json_bufer.write("]")
-        print("},")
+        #print("},")
         json_bufer.write("},\n")
-print("]")
+#print("]")
 json_bufer.write("]")
 json_bufer.close()
-
-#        json_bufer.write("],'lparam':'" +(splitted[0][2::][:-1]) + "'")
-
 
